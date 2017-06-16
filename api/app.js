200f5c41748e168811e460e9771b4c5e880c8232
Patrick Smith <patrick@burntcaramel.com>
2017-06-17 00:15:52 +1000

    Add JWT/CSRF support to api’s app

--- api/app.js
+++ api/app.js
@@ -17,6 +17,38 @@ server.connection({
   }
 })
 
-server.route(require('./routes'))
+server.register([
+	require('inert'),
+	require('hapi-auth-jwt2'),
+	{
+		register: require('crumb'),
+		options: {
+			restful: true,
+			cookieOptions: {
+				path: '/',
+				isHttpOnly: false, // Allow reading from front-end
+				isSecure: !isDev
+			}
+		}
+	}
+])
+.then(() => {
+	server.auth.strategy('auth0-jwt', 'jwt', {
+		key: process.env.AUTH0_CLIENT_SECRET,
+		validateFunc(decoded, request, callback) {
+			callback(null, true)
+		},
+		verifyOptions: {
+			algorithms: [ 'HS256' ],
+			audience: process.env.AUTH0_CLIENT_ID
+		},
+		urlKey: false,
+		cookieKey: false
+	})
+
+	server.auth.default('auth0-jwt')
+
+	server.route(require('./routes'))
+})
 
 server.start()
